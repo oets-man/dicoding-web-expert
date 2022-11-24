bac8cf374f8eaaae7f0b2950ac4b2e027c1c1113
Usman Husnan <oets.man@gmail.com>
2022-11-24 15:30:46 +0700

    grid

---
+++ canvas-menu/script.js
@@ -0,0 +1,11 @@
+const hamburgerButtonElement = document.querySelector('#hamburger');
+const drawerElement = document.querySelector('#drawer');
+hamburgerButtonElement.addEventListener('click', (event) => {
+	drawerElement.classList.toggle('open');
+	event.stopPropagation();
+});
+const mainElement = document.querySelector('main');
+mainElement.addEventListener('click', (event) => {
+	drawerElement.classList.remove('open');
+	event.stopPropagation();
+});
