8fe2f7ca76248f5672b2004d97a6de5f2a781bda
Usman Husnan <oets.man@gmail.com>
2022-11-25 10:31:48 +0700

    scss

--- dicoding-blog/webpack.config.js
+++ dicoding-blog/webpack.config.js
@@ -5,43 +5,41 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, 'dist/index.html'),
-      template: path.resolve(__dirname, 'src/index.html'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/images'),
-          to: path.resolve(__dirname, 'dist/images'),
-        },
-      ],
-    }),
-    new FaviconsWebpackPlugin({
-      logo: path.resolve(__dirname, 'src/images/dicoding.jpeg'),
-    }),
-    new CleanWebpackPlugin(),
-  ],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: path.resolve(__dirname, 'dist/index.html'),
+			template: path.resolve(__dirname, 'src/index.html'),
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/images'),
+					to: path.resolve(__dirname, 'dist/images'),
+				},
+			],
+		}),
+		new FaviconsWebpackPlugin({
+			logo: path.resolve(__dirname, 'src/images/dicoding.jpeg'),
+		}),
+		new CleanWebpackPlugin(),
+	],
 };
--- restaurant-apps/src/scripts/index.js
+++ restaurant-apps/src/scripts/index.js
@@ -1,4 +1,21 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+// import '../component/app-bar';
+import '../styles/main.scss';
 
-console.log('Hello Coders! :)');
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('nav');
+
+menu.addEventListener('click', function (event) {
+	drawer.classList.toggle('open-nav');
+	event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+	drawer.classList.remove('open-nav');
+});
+
+main.addEventListener('click', function () {
+	drawer.classList.remove('open-nav');
+});
