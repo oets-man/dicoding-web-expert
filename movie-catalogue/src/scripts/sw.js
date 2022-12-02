6a84a9ea3f36cdf3b95da8eea7b8e7d889560304
Usman Husnan <oets.man@gmail.com>
2022-12-02 22:58:32 +0700

    service worker native - done

--- movie-catalogue/src/scripts/sw.js
+++ movie-catalogue/src/scripts/sw.js
@@ -21,23 +21,19 @@ const assetsToCache = [
 
 self.addEventListener('install', (event) => {
 	// console.log('Installing Service Worker ...');
-
 	// TODO: Caching App Shell Resource
 	event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
 	// console.log('Activating Service Worker ...');
-
 	// TODO: Delete old caches
 	event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
 	// console.log(event.request);
-
 	// event.respondWith(fetch(event.request));
-
 	// TODO:
 	event.respondWith(CacheHelper.revalidateCache(event.request));
 });
