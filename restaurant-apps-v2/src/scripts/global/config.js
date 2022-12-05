81701aed071264f227c20b3c274a0599ede1540d
Usman Husnan <oets.man@gmail.com>
2022-12-05 09:03:32 +0700

    res2 - before idb

--- restaurant-apps-v2/src/scripts/global/config.js
+++ restaurant-apps-v2/src/scripts/global/config.js
@@ -2,6 +2,11 @@ const CONFIG = {
 	KEY: '12345',
 	BASE_URL: 'https://restaurant-api.dicoding.dev',
 	CACHE_NAME: 'RestaurantReview-V1',
+
+	// database idb
+	DATABASE_NAME: 'restaurant-review-database',
+	DATABASE_VERSION: 1,
+	OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
