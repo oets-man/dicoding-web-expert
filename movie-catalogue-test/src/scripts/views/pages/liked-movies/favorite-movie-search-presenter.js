54b07ca6ec096e05679607a6f079a5c1d7c87c95
Usman Husnan <oets.man@gmail.com>
2022-12-10 14:30:24 +0700

    movie 2 - class FavoriteMovieSearchView - error

---
+++ movie-catalogue-test/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -0,0 +1,50 @@
+class FavoriteMovieSearchPresenter {
+	constructor({ favoriteMovies, view }) {
+		this._view = view;
+		this._listenToSearchRequestByUser();
+		this._favoriteMovies = favoriteMovies;
+	}
+
+	_listenToSearchRequestByUser() {
+		// this._queryElement = document.getElementById('query');
+		// this._queryElement.addEventListener('change', (event) => {
+		// 	this._searchMovies(event.target.value);
+		// });
+		this._view.runWhenUserIsSearching((latestQuery) => {
+			this._searchMovies(latestQuery);
+		});
+	}
+
+	async _searchMovies(latestQuery) {
+		this._latestQuery = latestQuery.trim();
+		let foundMovies;
+		if (this.latestQuery.length > 0) {
+			foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+		} else {
+			foundMovies = await this._favoriteMovies.getAllMovies();
+		}
+
+		// const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+
+		this._showFoundMovies(foundMovies);
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	_showFoundMovies(movies) {
+		let html;
+		if (movies.length > 0) {
+			html = movies.reduce((carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`), '');
+		} else {
+			html = '<div class="movies__not__found">Film tidak ditemukan</div>';
+		}
+
+		document.querySelector('.movies').innerHTML = html;
+		document.getElementById('movie-search-container').dispatchEvent(new Event('movies:searched:updated'));
+	}
+
+	get latestQuery() {
+		return this._latestQuery;
+	}
+}
+
+export default FavoriteMovieSearchPresenter;
