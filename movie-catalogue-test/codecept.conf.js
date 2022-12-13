183b800cc25e793df141706c263da02c878c08bb
Usman Husnan <oets.man@gmail.com>
2022-12-13 19:40:01 +0700

    new file:   movie-catalogue-test/e2e/outputs/Liking_Movies_before_each_hook__Before_for_showing_empty_liked_movies.failed.png new file:   movie-catalogue-test/e2e/outputs/cli-history new file:   movie-catalogue-test/e2e/outputs/liking_one_movie.failed.png new file:   movie-catalogue-test/e2e/outputs/searching_movies.failed.png new file:   movie-catalogue-test/e2e/outputs/showing_empty_liked_movies.failed.png

--- movie-catalogue-test/codecept.conf.js
+++ movie-catalogue-test/codecept.conf.js
@@ -8,17 +8,17 @@ setCommonPlugins();
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: 'e2e/**/*.spec.js',
-  output: 'e2e/outputs',
-  helpers: {
-    Puppeteer: {
-      url: 'http://127.0.0.1:9000/',
-      show: true,
-      windowSize: '1200x900'
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  name: 'movie-catalogue-test'
-}
+	tests: 'e2e/**/*.spec.js',
+	output: 'e2e/outputs',
+	helpers: {
+		Puppeteer: {
+			url: 'http://127.0.0.1:9000/',
+			show: true,
+			windowSize: '1200x900',
+		},
+	},
+	include: {
+		I: './steps_file.js',
+	},
+	name: 'movie-catalogue-test',
+};
