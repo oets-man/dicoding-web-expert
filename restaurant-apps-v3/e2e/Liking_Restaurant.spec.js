4feb0b01d5b9c4e98d6b6f872dbad85e3ccfffcf
Usman Husnan <oets.man@gmail.com>
2022-12-21 16:28:36 +0700

    pierce

--- restaurant-apps-v3/e2e/Liking_Restaurant.spec.js
+++ restaurant-apps-v3/e2e/Liking_Restaurant.spec.js
@@ -11,7 +11,59 @@ Before(({ I }) => {
 Scenario('liking one restaurant', async ({ I }) => {
 	I.waitForElement('.error', 10); // secs
 	I.see('Tidak ada data untuk ditampilkan. Silakan favoritkan beberapa restoran!', '.error');
-	I.amOnPage('./');
+
+	// --------------------------------
+	// I.amOnPage('./');
+	// I.seeElement('my-card');
+	// I.click(locate('my-card').first());
+	// I.waitForVisible('pierce/.description', 5);
+	// I.waitForVisible('pierce/.detail', 5);
+	// I.click('pierce/.detail');
+	// pause();
+
+	// --------------------------------
+	for (let i = 1; i <= 3; i++) {
+		I.amOnPage('./');
+		I.waitForElement('my-card', 30);
+		I.click(locate('my-card').at(i));
+		const nameInCard = await I.grabTextFrom('pierce/.item-name');
+		console.log(i, nameInCard); //loop pertama dan kedua nilainya sama
+		I.waitForVisible('pierce/.description', 5); // looping kedua berhenti di sini
+		I.waitForVisible('pierce/.detail', 5);
+		I.click('pierce/.detail');
+		I.waitForElement('h3', 10);
+		const nameInDetail = await I.grabTextFrom('h3');
+		assert.strictEqual(nameInCard, nameInDetail);
+	}
+
+	// --------------------------------
+	// I.amOnPage('./');
+	// puppeteer.registerCustomQueryHandler('shadow', QueryHandler);
+	// I.waitForElement('my-card', 30); // secs
+	// I.click(locate('my-card').first());
+	// const nameInCard = await I.grabTextFrom('shadow/.item-name');
+	// I.waitForVisible('shadow/.description', 5);
+	// I.waitForVisible('shadow/.detail', 5);
+	// I.click('shadow/.detail');
+	// I.waitForElement('h3', 10);
+	// const nameInDetail = await I.grabTextFrom('h3');
+	// assert.strictEqual(nameInCard, nameInDetail);
+	// --------------------------------
+	// for (let i = 1; i <= 3; i++) {
+	// 	I.amOnPage('./');
+	// 	const shadow = 'shadow' + String.fromCharCode(i + 64);
+	// 	puppeteer.registerCustomQueryHandler(shadow, QueryHandler);
+	// 	I.waitForElement('my-card', 30);
+	// 	I.click(locate('my-card').at(i));
+	// 	const nameInCard = await I.grabTextFrom(`${shadow}/.item-name`);
+	// 	console.log(i, nameInCard) //loop pertama dan kedua nilainya sama
+	// 	I.waitForVisible(`${shadow}/.description`, 5); // looping kedua berhenti di sini
+	// 	I.waitForVisible(`${shadow}/.detail`, 5);
+	// 	I.click(`${shadow}/.detail`);
+	// 	I.waitForElement('h3', 10);
+	// 	const nameInDetail = await I.grabTextFrom('h3');
+	// 	assert.strictEqual(nameInCard, nameInDetail);
+	// }
 
 	// I.waitForElement('my-card', 10); // secs
 	// I.click(locate('my-card').first());
@@ -29,53 +81,10 @@ Scenario('liking one restaurant', async ({ I }) => {
 	// const restaurant = await I.grabTextFrom('h3');
 	// assert.strictEqual(name, restaurant);
 
-	const restaurants = [];
-	for (let i = 1; i <= 3; i++) {
-		let shadow ='shadow'
-		switch(i) {
-			case 1:
-			  shadow='shadowa'
-			  break;
-			case 2:
-			  shadow='shadowb'
-			  break;
-			default:
-			  shadow='shadowc'
-		  }
-		  console.log(shadow);
-		puppeteer.registerCustomQueryHandler(shadow, QueryHandler);
-		I.waitForElement('my-card', 30); // secs
-		I.click(locate('my-card').at(i));
-		const itemName = `${shadow}/.item-name`;
-		const description = `${shadow}/.description`;
-		const detail = `${shadow}/.detail`;
-
-		const name = await I.grabTextFrom(itemName);
-		console.log('name ' + name);
-
-		I.waitForVisible(detail, 5);
-		I.waitForVisible(description, 5);
-		I.click(detail);
-		I.waitForElement('h3', 10);
-		let restaurant = await I.grabTextFrom('h3');
-		console.log('restaurant ' + restaurant);
-		// assert.strictEqual(name, restaurant);
-		I.amOnPage('./');
-		pause();
-	}
-
 	// pause();
 	// I.waitForVisible('div.description', 5);
 	// I.waitForVisible({ shadow: ['my-card', 'div.item', 'div.description'] }, 5);
 	// pause();
-
-	// for (let i = 1; i <= 3; i++) {
-	// 	I.amOnPage('./');
-	// 	console.log(i);
-	// 	I.waitForElement('my-card', 30); // secs
-	// 	I.click(locate('my-card').at(i));
-	// 	I.amOnPage('./#/favorite');
-	// }
 
 	// const restaurants = [];
 	// for (let i = 1; i <= 3; i++) {
